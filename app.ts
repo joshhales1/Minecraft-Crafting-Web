1ce177edd874aff1a40c0566e8b3207bd06bef71
JayHales <joshhales2004@gmail.com>
2021-02-22 21:35:24 +0000

    Implemented item structure.
    Similar to tag implementation.

--- app.ts
+++ app.ts
@@ -4,12 +4,15 @@ const fs = require('fs');
 
 
 const TAGS_LOCATION: string = './minecraft-data/tags/items/';
+const RECIPES_LOCATION: string = './minecraft-data/recipes/';
+
 const tags: object = {};
 const items: object = {};
 
 loadTags();
+loadItems();
 
-console.log(tags);
+console.log(items['minecraft:activator_rail']);
 
 function loadTags() {
     let files: string[] = fs.readdirSync(TAGS_LOCATION);
@@ -30,14 +33,61 @@ function loadTags() {
 
                 return;
             }
-
             mainTag.addChildItem(getItem(value));
             tags[mainTag.name] = mainTag;
         });      
     });
 }
 
+function loadItems() {
+    let files: string[] = fs.readdirSync(RECIPES_LOCATION);
+
+    files.forEach(file => {
+        let item: object = JSON.parse(
+            fs.readFileSync(RECIPES_LOCATION + file)
+        );
+
+        if (item['type'].startsWith('minecraft:crafting_special'))
+            return;
+
+        let result = getItem(item['result']['item']);
+
+        if (item['ingredients'] !== undefined) {
+            item['ingredients'].forEach(ingredient => {
+                getItem(ingredient['item']).addChildItem(result);
+            });
+
+            return;
+        }
+
+        if (item['ingredient'] !== undefined) {
+            getItem(item['ingredient']['item']).addChildItem(result);
+
+            return;
+        }
+
+        if (item['type'] === 'minecraft:smithing') {
+            getItem(item['base']['item']).addChildItem(result);
+            getItem(item['addition']['item']).addChildItem(result);
+
+            return;
+        }
+
+        if (item['type'] === 'minecraft:crafting_shaped') {
+
+            Object.keys(item['key']).forEach(key => {
+
+                getItem(item['key'][key]['item']).addChildItem(result);
+            })
+
+            return;
+        }
+    });
+}
+
 function getItem(name: string): Item {
+
+
     if (!(name in items)) {
         let newItem: Item = new Item(name);
         items[name] = newItem;
